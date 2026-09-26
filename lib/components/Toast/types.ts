7ce8b0d11a061850905ab18d191c96f0a4d4ee66
Aylong <alexanderkitsa@gmail.com>
2026-09-26 07:49:15 +0300

    Toast types

--- lib/components/Toast/types.ts
+++ lib/components/Toast/types.ts
@@ -2,19 +2,24 @@ import type { ReactNode } from 'react';
 import type { CssColors } from 'tgui-core/common/constants';
 import type { IconProps } from '../Icon/types';
 
-export type ToastProps = Partial<{
-  /** Toast class for CSS customization */
-  className: string;
-  /** Toast accent color */
-  color: CssColors;
+export type ToastProps = {
   /** Bold text on top */
   title: ReactNode;
+} & Partial<{
   /** Content under title */
   content: ReactNode;
-  /** Additional buttons inside toast */
-  buttons: ReactNode;
   /** Any FontAwesome icon */
   icon: IconProps;
-  /** Determies how long toast will be visible, until auto-dismiss in ms. */
+  /** Toast accent color */
+  color: CssColors;
+  /** Additional buttons inside toast */
+  buttons: ReactNode;
+  /** Toast class for CSS customization */
+  className: string;
+  /**
+   * Determies how long toast will be visible,
+   * until auto-dismiss in ms.
+   * Defauilt is 5000 - 5s
+   */
   duration: number;
 }>;
